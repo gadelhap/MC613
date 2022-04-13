7793ab498acd28db543577d2b7567657bab32d53
Pedro Henrique Pinheiro Gadelha <ra186985@metalica.lab.ic.unicamp.br>
2022-04-13 17:08:23 -0300

    foda-se parece ok

--- Lab04/alu.vhd
+++ Lab04/alu.vhd
@@ -11,6 +11,29 @@ entity alu is
 end alu;
 
 architecture behavioral of alu is
+	signal sum_b: std_logic_vector(3 downto 0);
+	signal sum_F: std_logic_vector(3 downto 0);
+	signal sum_C: std_logic;
+	signal sum_V: std_logic;
+	signal aux_F: std_logic_vector(3 downto 0);
 begin
-  -- add your code
+	sum_b <= not b when (s0 = '1' and s1 = '0') else
+					b;
+	adder: entity work.ripple_carry 
+				port map(x => a, y => sum_b,
+					r => sum_F,
+					cin => s0, cout => sum_C,
+					overflow => sum_V);
+	aux_F <= (a and b) when (s0 = '0' and s1 = '1') else
+			(a or b) when (s0 = '1' and s1 = '1') else
+			sum_F;
+	C <= sum_C when (s1 = '0') else
+			'0';
+	V <= sum_V when (s1 = '0') else
+			'0';
+	N <= aux_F(3) when (s1 = '0') else
+			'0';
+	Z <= '1' when (aux_F = "0000") else
+			'0';
+	F <= aux_F;
 end behavioral;
--- Lab04/ripple_carry.vhd
+++ Lab04/ripple_carry.vhd
@@ -18,11 +18,11 @@ architecture rtl of ripple_carry is
   signal c: std_logic_vector(0 to N);
 begin
   c(0) <= cin;
-  for i in 0 to N - 1 generate:
+  gen_label: for i in 0 to N - 1 generate
     stagei: entity work.full_adder port map(x => x(i), y => y(i),
                                             r => r(i),
-                                            cin => c(i), cout => c(i + 1))
+                                            cin => c(i), cout => c(i + 1));
   end generate;
   cout <= c(N);
-  overflow <= c(N - 1) xor c(N)
+  overflow <= c(N - 1) xor c(N);
 end rtl;

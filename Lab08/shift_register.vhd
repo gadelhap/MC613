98082059aed7ec14239a7664e74f0d4d0628b25c
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2022-05-11 15:13:33 -0300

    update do ser in

--- Lab08/shift_register.vhd
+++ Lab08/shift_register.vhd
@@ -19,17 +19,17 @@ begin
   begin
     wait until clk'event and clk = '1';
   
-    if mode = '01' AND ser_in = '1' then
+    if mode = '01' then
       for i in 1 to N-1 loop
         par_out(i) <= par_in(i-1);
       end loop;
-      par_out(0) <= par_in(N-1);
+      par_out(0) <= ser_in;
         
-    elsif mode = '10' AND ser_in = '1' then
+    elsif mode = '10' then
       for i in 0 to N-2 loop
         par_out(i) <= par_in(i+1);
       end loop;
-      par_out(N-1) <= par_in(0);
+      par_out(N-1) <= ser_in;
       
     elsif mode = '11' then
       par_out <= par_in;
